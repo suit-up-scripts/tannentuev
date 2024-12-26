c9ac3683ce2fa8e075e8f59aa2c6be0b5a701fa6
Stefan Litzel <suit-up@live.de>
2024-12-26 16:03:16 +0100

    changed current year to 2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tannentuev';
-  activeYear: number = 2023;
+  activeYear: number = 2024;
 
 
   onYearChanged(year: number) {
